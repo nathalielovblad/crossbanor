c3135593db4ee05642eba1f31f38624f2936d0c6
nathalielovblad <nathalie.lovblad@gmail.com>
2026-06-26 14:43:37 +0200

    Update scrape_mx.py

--- scrape_mx.py
+++ scrape_mx.py
@@ -18,9 +18,14 @@ from bs4 import BeautifulSoup
 
 HEADERS = {
     "User-Agent": (
-        "Mozilla/5.0 (compatible; MXSthlmBot/1.0; "
-        "+https://din-domän.se kontakta@din-domän.se)"
-    )
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/125.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "sv-SE,sv;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
 }
 
 NORMALA_TIDER = {
